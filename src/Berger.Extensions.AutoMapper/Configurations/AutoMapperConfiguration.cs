6131665c1c34587c2a0d1363af7ed75ee1e86f61
Rafael Cronemberger <rafael@berger.com.br>
2023-11-27 03:05:16 -0300

    Package updates

--- src/Berger.Extensions.AutoMapper/Configurations/AutoMapperConfiguration.cs
+++ src/Berger.Extensions.AutoMapper/Configurations/AutoMapperConfiguration.cs
@@ -6,7 +6,8 @@ namespace Berger.Extensions.AutoMapper
     {
         public static void ConfigureAutoMapper<Startup, TSource, TDestination>(this IServiceCollection services)
         {
-            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (services is null)
+                throw new ArgumentNullException(nameof(services));
 
             services.ConfigureStartup<Startup>();
 
@@ -14,13 +15,13 @@ namespace Berger.Extensions.AutoMapper
         }
         public static void ConfigureAutoMapper<Startup, D, I, V>(this IServiceCollection services)
         {
-            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (services is null)
+                throw new ArgumentNullException(nameof(services));
 
             services.ConfigureStartup<Startup>();
 
             services.AddAutoMapper(typeof(D), typeof(I), typeof(V));
         }
-
         private static void ConfigureStartup<T>(this IServiceCollection services)
         {
             services.AddAutoMapper(typeof(T));
